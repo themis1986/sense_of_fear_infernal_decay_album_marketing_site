8f04fd79e013ae7adf681c190755e05934251163
Themis <tiakovidis@kpler.com>
2025-11-26 10:54:52 +0200

    refactor vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: "/sense_of_fear_infernal_decay_album_marketing_site/",
+  base: "/sense_of_fear_infernal_decay_album_marketing_site/",
   plugins: [react()],
 });
